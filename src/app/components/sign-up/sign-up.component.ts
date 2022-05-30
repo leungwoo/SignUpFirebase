1fbc61e22bf88d72e3c58b1f494a246bc418f0bc
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2022-05-29 22:16:48 -0400

    SignUp component completed

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,4 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormControl, Validators, ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HotToastService } from '@ngneat/hot-toast';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+
+export function passwordMatchValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+  const password = control.get('password')?.value;
+  const confirmpassword = control.get('confirmpassword')?.value;
+
+  if(password && confirmpassword && password !== confirmpassword) {
+    return {
+      passwordsDontMatch:true
+    }
+  } 
+  return null;
+};
+}
 
 @Component({
   selector: 'app-sign-up',
@@ -6,7 +24,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.css'],
 })
 export class SignUpComponent implements OnInit {
-  constructor() {}
+  signUpForm = new FormGroup ({
+    name:new FormControl('',[Validators.required]),
+    email: new FormControl('',[Validators.required,Validators.email]),
+    password: new FormControl('',[Validators.required]),
+    confirmpassword: new FormControl('',[Validators.required])
+
+  },{validators:passwordMatchValidator()});
+  constructor(private authService:AuthenticationService, private router:Router, private toast:HotToastService) {}
 
   ngOnInit(): void {}
+
+  get name(){
+   return this.signUpForm.get('name');
+  }
+  get email(){
+    return this.signUpForm.get('email');
+  }
+  get password(){
+    return this.signUpForm.get('password');
+  }
+  get confirmpassword(){
+    return this.signUpForm.get('confirmpassword');
+  }
+  onSubmit(){
+    if(this.signUpForm.invalid){
+      return;
+    }
+    const {name, email, password}=this.signUpForm.value;
+    this.authService.signUp(name,email,password).pipe(
+      this.toast.observe({
+        success:'Congrats you are Signed Up!',
+      loading:'Signning you up..',
+    error:'Sorry there was an error '
+  })
+    ).subscribe(()=>{
+      this.router.navigateByUrl('/home')
+    })
+  }
 }
--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Auth, authState, signInWithEmailAndPassword } from '@angular/fire/auth';
-import { from } from 'rxjs';
+import { createUserWithEmailAndPassword, updateProfile } from '@firebase/auth';
+import { from,switchMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +10,15 @@ export class AuthenticationService {
 
   constructor(private auth:Auth) {}
    currentUser$= authState(this.auth); //observable returns a user
-   
+
   login(username:string,password:string){
     return from(signInWithEmailAndPassword(this.auth,username,password));
   }
   logout(){
     return from(this.auth.signOut());
+  }
+  signUp(name:string, email:string, password:string){
+    return from(createUserWithEmailAndPassword(this.auth,email,password)).pipe(switchMap(({user})=>updateProfile(user,{displayName:name})));
   }
 }
 //converted to a observable with 'from'
